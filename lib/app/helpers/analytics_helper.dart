b8a8e569e8862be457ae3fa0b900b6890045a37d
DANIS <danis.preldzic@gmail.com>
2025-11-06 21:21:32 +0100

    analytics 99%

---
+++ lib/app/helpers/analytics_helper.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class RevenueChartStyle {
+  final Color lineColor;
+  final Color fillColor;
+  final Color highlightColor;
+  final Color gridColor;
+  final Color labelColor;
+  final Color tooltipBackgroundColor;
+  final Color tooltipTextColor;
+  final double lineWidth;
+  final double dotSize;
+  final double highlightDotSize;
+
+  const RevenueChartStyle({
+    this.lineColor = const Color(0xFF0F766E), // Teal
+    this.fillColor = const Color(0x1A0F766E), // Teal with opacity
+    this.highlightColor = const Color(0xFF0F766E),
+    this.gridColor = const Color(0xFFE5E7EB),
+    this.labelColor = const Color(0xFF6B7280),
+    this.tooltipBackgroundColor = const Color(0xFF0F766E),
+    this.tooltipTextColor = Colors.white,
+    this.lineWidth = 2.0,
+    this.dotSize = 4.0,
+    this.highlightDotSize = 8.0,
+  });
+}
--- lib/main_common.dart
+++ lib/main_common.dart
@@ -9,6 +9,7 @@ import 'package:w_permissions_module/services/locator.dart';
 import 'package:w_utils/helper/util_constants.dart';
 import 'package:w_utils/providers/theme_provider/whiskr_web_theme/custom_web_themes.dart';
 import 'package:w_utils/w_utils.dart';
+import 'package:wa_analytics_module/providers/wa_analytics_provider.dart';
 import 'package:wa_inventory_services_module/providers/wa_inventory_services_provider.dart';
 import 'package:wa_onboarding_module/providers/wa_onboarding_provider.dart';
 import 'package:whiskr_admin_panel/app/utils/session_manager.dart';
@@ -81,6 +82,7 @@ class WhiskrAdminApp extends StatelessWidget {
         ChangeNotifierProvider<WAOnboardingProvider>(create: (_) => WAOnboardingProvider(), lazy: true),
         ChangeNotifierProvider<ImageHandleProvider>(create: (_) => ImageHandleProvider(), lazy: true),
         ChangeNotifierProvider<WAInventoryServicesProvider>(create: (_) => WAInventoryServicesProvider(), lazy: true),
+        ChangeNotifierProvider<WAAnalyticsProvider>(create: (_) => WAAnalyticsProvider(), lazy: true),
       ],
       child: Consumer<CustomThemeProvider>(
         builder: (context, customThemeProvider, child) {
